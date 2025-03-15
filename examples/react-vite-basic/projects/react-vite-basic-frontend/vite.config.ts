c17ae9506c454b3ff616fdad0ae1780430e86a5a
David Rojas <lempira@gmail.com>
2025-03-15 19:57:29 -0400

    fix: updated react vite template to allow for BASE_PATH env var

--- examples/react-vite-basic/projects/react-vite-basic-frontend/vite.config.ts
+++ examples/react-vite-basic/projects/react-vite-basic-frontend/vite.config.ts
@@ -1,15 +1,19 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      globals: {
-        Buffer: true,
-      },
-    }),
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      nodePolyfills({
+        globals: {
+          Buffer: true,
+        },
+      }),
+    ],
+    base: env.BASE_PATH || "",
+  };
+});
